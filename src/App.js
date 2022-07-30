e69a778746ce09d1046c5657aaaac0a795b0badb
Azeem <livelyazeem@gmail.com>
2022-07-30 12:55:07 +0500

    coded initial structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import './style.scss';
 
+const Buttons = (props) => {
+    return (
+        <div id="buttonsContainer">
+            <div>
+                <button>Q</button>
+                <button>W</button>
+                <button>E</button>
+            </div>
+            <div>
+                <button>A</button>
+                <button>S</button>
+                <button>D</button>
+            </div>
+            <div>
+                <button>Z</button>
+                <button>X</button>
+                <button>C</button>
+            </div>
+        </div>
+    );
+};
 class App extends React.Component {
     constructor(props) {
         super(props);
@@ -9,7 +30,9 @@ class App extends React.Component {
 
     render() {
         return (
-            <div></div>
+            <div id="drum-machine">
+                <Buttons />
+            </div>
         );
     }
 }
