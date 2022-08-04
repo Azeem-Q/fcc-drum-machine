f63a5be852fcb2bee932b641f88fa2f860ec8e76
Azeem <livelyazeem@gmail.com>
2022-08-04 12:30:51 +0500

    completed user story#5

--- src/App.js
+++ src/App.js
@@ -5,19 +5,37 @@ const Buttons = (props) => {
     return (
         <div id="display">
             <div>
-                <button id="Q" className="drum-pad">Q</button>
-                <button id="W" className="drum-pad">W</button>
-                <button id="E" className="drum-pad">E</button>
+                <button id="Q" className="drum-pad">
+                    <audio id="Q" className="clip" src=""></audio>Q
+                </button>
+                <button id="W" className="drum-pad">
+                    <audio id="W" className="clip" src=""></audio>W
+                </button>
+                <button id="E" className="drum-pad">
+                    <audio id="E" className="clip" src=""></audio>E
+                </button>
             </div>
             <div>
-                <button id="A" className="drum-pad">A</button>
-                <button id="S" className="drum-pad">S</button>
-                <button id="D" className="drum-pad">D</button>
+                <button id="A" className="drum-pad">
+                    <audio id="A" className="clip" src=""></audio>A
+                </button>
+                <button id="S" className="drum-pad">
+                    <audio id="S" className="clip" src=""></audio>S
+                </button>
+                <button id="D" className="drum-pad">
+                    <audio id="D" className="clip" src=""></audio>D
+                </button>
             </div>
             <div>
-                <button id="Z" className="drum-pad">Z</button>
-                <button id="X" className="drum-pad">X</button>
-                <button id="C" className="drum-pad">C</button>
+                <button id="Z" className="drum-pad">
+                    <audio id="Z" className="clip" src=""></audio>Z
+                </button>
+                <button id="X" className="drum-pad">
+                    <audio id="X" className="clip" src=""></audio>X
+                </button>
+                <button id="C" className="drum-pad">
+                    <audio id="C" className="clip" src=""></audio>C
+                </button>
             </div>
         </div>
     );
