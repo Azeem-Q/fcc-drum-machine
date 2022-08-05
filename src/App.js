16b488cf1fda04a7ed730ecfae71c46619deb707
Azeem <livelyazeem@gmail.com>
2022-08-05 12:23:02 +0500

    completed user story#5

--- src/App.js
+++ src/App.js
@@ -5,36 +5,117 @@ const Buttons = (props) => {
     return (
         <div id="display">
             <div>
-                <button id="Q" className="drum-pad">
-                    <audio id="Q" className="clip" src=""></audio>Q
+                <button
+                    id="QA"
+                    className="drum-pad"
+                    onClick={() => document.getElementById('Q').play()}
+                >
+                    <audio
+                        id="Q"
+                        className="clip"
+                        src="https://www.soundjay.com/nature/sounds/rain-03.mp3"
+                    ></audio>
+                    Q
                 </button>
-                <button id="W" className="drum-pad">
-                    <audio id="W" className="clip" src=""></audio>W
+                <button
+                    id="WA"
+                    className="drum-pad"
+                    onClick={() => document.getElementById('W').play()}
+                >
+                    <audio
+                        id="W"
+                        className="clip"
+                        src="https://www.soundjay.com/nature/sounds/rain-03.mp3"
+                    ></audio>
+                    W
                 </button>
-                <button id="E" className="drum-pad">
-                    <audio id="E" className="clip" src=""></audio>E
+                <button
+                    id="EA"
+                    className="drum-pad"
+                    onClick={() => document.getElementById('E').play()}
+                >
+                    <audio
+                        id="E"
+                        className="clip"
+                        src="https://www.soundjay.com/nature/sounds/rain-03.mp3"
+                    ></audio>
+                    E
                 </button>
             </div>
             <div>
-                <button id="A" className="drum-pad">
-                    <audio id="A" className="clip" src=""></audio>A
+                <button
+                    id="AA"
+                    className="drum-pad"
+                    onClick={() => document.getElementById('A').play()}
+                >
+                    <audio
+                        id="A"
+                        className="clip"
+                        src="https://www.soundjay.com/nature/sounds/rain-03.mp3"
+                    ></audio>
+                    A
                 </button>
-                <button id="S" className="drum-pad">
-                    <audio id="S" className="clip" src=""></audio>S
+                <button
+                    id="SA"
+                    className="drum-pad"
+                    onClick={() => document.getElementById('S').play()}
+                >
+                    <audio
+                        id="S"
+                        className="clip"
+                        src="https://www.soundjay.com/nature/sounds/rain-03.mp3"
+                    ></audio>
+                    S
                 </button>
-                <button id="D" className="drum-pad">
-                    <audio id="D" className="clip" src=""></audio>D
+                <button
+                    id="DA"
+                    className="drum-pad"
+                    onClick={() => document.getElementById('D').play()}
+                >
+                    <audio
+                        id="D"
+                        className="clip"
+                        src="https://www.soundjay.com/nature/sounds/rain-03.mp3"
+                    ></audio>
+                    D
                 </button>
             </div>
             <div>
-                <button id="Z" className="drum-pad">
-                    <audio id="Z" className="clip" src=""></audio>Z
+                <button
+                    id="ZA"
+                    className="drum-pad"
+                    onClick={() => document.getElementById('Z').play()}
+                >
+                    <audio
+                        id="Z"
+                        className="clip"
+                        src="https://www.soundjay.com/nature/sounds/rain-03.mp3"
+                    ></audio>
+                    Z
                 </button>
-                <button id="X" className="drum-pad">
-                    <audio id="X" className="clip" src=""></audio>X
+                <button
+                    id="XA"
+                    className="drum-pad"
+                    onClick={() => document.getElementById('X').play()}
+                >
+                    <audio
+                        id="X"
+                        className="clip"
+                        src="https://www.soundjay.com/nature/sounds/rain-03.mp3"
+                    ></audio>
+                    X
                 </button>
-                <button id="C" className="drum-pad">
-                    <audio id="C" className="clip" src=""></audio>C
+                <button
+                    id="CA"
+                    className="drum-pad"
+                    onClick={() => document.getElementById('C').play()}
+                >
+                    <audio
+                        id="C"
+                        className="clip"
+                        src="https://www.soundjay.com/nature/sounds/rain-03.mp3"
+                    ></audio>
+                    C
                 </button>
             </div>
         </div>
@@ -44,6 +125,12 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {};
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e) {
+        const audio = document.getElementById('Q');
+        audio.play();
     }
 
     render() {
